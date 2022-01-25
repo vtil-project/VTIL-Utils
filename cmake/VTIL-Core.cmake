9677e89eee5efbcae418d780720415f673daad5e
Duncan Ogilvie <mr.exodia.tpodt@gmail.com>
2022-01-25 03:44:26 +0100

    Point to a specific commit on vtil-project/VTIL-Core

--- cmake/VTIL-Core.cmake
+++ cmake/VTIL-Core.cmake
@@ -1,5 +1,5 @@
 CPMAddPackage(
     NAME VTIL-Core
-    GITHUB_REPOSITORY z3t4s/VTIL-Core
-    GIT_TAG master
+    GITHUB_REPOSITORY vtil-project/VTIL-Core
+    GIT_TAG 0e1062b61cceea5f43e4238a6c66e0e4afdf5d98
 )
